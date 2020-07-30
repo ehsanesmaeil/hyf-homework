dc63ea98540ec824a760b5712bf9acc083c1d343
ehsanesmaeil <ehsanesmaeilzadeh82@gmail.com>
2020-07-30 10:23:20 +0200

    adding comments for separating different parts

--- javascript/javascript1/week1/index.js
+++ javascript/javascript1/week1/index.js
@@ -1,57 +1,72 @@
-
-function agify(){
+// ---------------- code for Agify
+function agify() {
   var yearOfBirth = window.prompt("Please, enter your year of birth");
   var yearFuture = window.prompt("Which year in future?");
   var age = yearFuture - yearOfBirth;
-  return "You will be " + age + " years old at 2050."
+  return "You will be " + age + " years old at 2050.";
 }
 
 console.log(agify());
 
-function dogAgify(){
+// ---------------- code for dog Agify
+
+function dogAgify() {
   var dogYearOfBirth = window.prompt("Please, enter your dog's year of birth");
   var dogYearFuture = window.prompt("Which year in future?");
-  var dogOrHuman = window.prompt("DO you like to calculate in dog year or human? (d or h) ")
-      if (dogOrHuman !="d" || dogOrHuman !="h"  ){
-          dogOrHuman = window.prompt("Please just enter d or h!) ")
-      }
-      if (dogOrHuman == "d"){
-          return (dogYearFuture - dogYearOfBirth)*7;
-      }else if (dogOrHuman == "h"){
-          return dogYearFuture - dogYearOfBirth;
-      }
-
+  var dogOrHuman = window.prompt(
+    "DO you like to calculate in dog year or human? (d or h) "
+  );
+  if (dogOrHuman != "d" || dogOrHuman != "h") {
+    dogOrHuman = window.prompt("Please just enter d or h!) ");
   }
-console.log(dogAgify())
+  if (dogOrHuman == "d") {
+    return (dogYearFuture - dogYearOfBirth) * 7;
+  } else if (dogOrHuman == "h") {
+    return dogYearFuture - dogYearOfBirth;
+  }
+}
+console.log(dogAgify());
 
-function housePricing(){
-
+// ---------------- code for house Price
+function housePricing() {
   var width = window.prompt("What is the width in meter?");
   var depth = window.prompt("What is the depth in meter? ");
   var height = window.prompt("What is the hight in meter? ");
-  var volumeInMeters = width*depth*height;
-  var gardenSizeInM2 = window.prompt("What the size of garden in square meter?");
+  var volumeInMeters = width * depth * height;
+  var gardenSizeInM2 = window.prompt(
+    "What the size of garden in square meter?"
+  );
   var price = window.prompt("How much did you pay? ");
-  var housePrice = volumeInMeters*2.5*1000 + gardenSizeInM2;
+  var housePrice = volumeInMeters * 2.5 * 1000 + gardenSizeInM2;
 
-      if (price<= housePrice){
-          return "It's a fair price!"
-      } else{
-          return "It's not logical to buy it!"
-      }
+  if (price <= housePrice) {
+    return "It's a fair price!";
+  } else {
+    return "It's not logical to buy it!";
   }
+}
 
-console.log(housePricing())
+console.log(housePricing());
 
-
+// ---------------- code for house Random StartUp name
 
 function startUpName() {
-var firstWords = ["easy", "Try", "Awesome", "Bing", "SoSo", "mindy", "wow!"];
-var secondWOrds = [ "US","StartUp","Face", "Medium","Done","Studio","Corporation"];
+  var firstWords = ["easy", "Try", "Awesome", "Bing", "SoSo", "mindy", "wow!"];
+  var secondWOrds = [
+    "US",
+    "StartUp",
+    "Face",
+    "Medium",
+    "Done",
+    "Studio",
+    "Corporation",
+  ];
 
-var startUpName = firstWords[Math.floor((Math.random() * firstWords.length) + 0)] + " " +secondWOrds[Math.floor((Math.random() * firstWords.length) + 0)];
-return startUpName;
-
+  var startUpName =
+    firstWords[Math.floor(Math.random() * firstWords.length + 0)] +
+    " " +
+    secondWOrds[Math.floor(Math.random() * firstWords.length + 0)];
+  return startUpName;
 }
 
 console.log(startUpName());
